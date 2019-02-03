7d12cc32064306bdd8d168a4cc5b13016568c0b6
Nate Bierdeman <natebierdeman@gmail.com>
2019-02-03 12:00:40 -0600

    Set freqs default value by sending it to ::new

--- char_counter.rb
+++ char_counter.rb
@@ -1,7 +1,6 @@
 # outputs character frequencies of a string
 def char_counter(string)
-  freqs = {}
-  freqs.default = 0
+  freqs = Hash.new(0)
   string.downcase.chars.sort.join.each_char { |char| freqs[char] += 1 }
   freqs.each { |char, count| puts "#{char}: #{count}" }
 end
