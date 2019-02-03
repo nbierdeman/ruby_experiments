f675a7b042d65a71ee488594c019fe56dd5f61ea
Nate Bierdeman <natebierdeman@gmail.com>
2019-02-03 11:55:40 -0600

    Remove string.downcase bang

--- char_counter.rb
+++ char_counter.rb
@@ -2,6 +2,6 @@
 def char_counter(string)
   freqs = {}
   freqs.default = 0
-  string.downcase!.chars.sort.join.each_char { |char| freqs[char] += 1 }
+  string.downcase.chars.sort.join.each_char { |char| freqs[char] += 1 }
   freqs.each { |char, count| puts "#{char}: #{count}" }
 end
